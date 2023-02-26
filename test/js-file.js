a6dfc237d5569177747106a543700d933bbf542c
Jerrick Backous <77847817+JerrickBackous@users.noreply.github.com>
2023-02-26 15:04:39 -0600

    manipulate DOM

--- test/js-file.js
+++ test/js-file.js
@@ -30,7 +30,32 @@ divParent.appendChild(divHeader);
 const divParagraph = document.createElement('p');
 divParagraph.classList.add('divParagraphBlack');
 divParagraph.textContent = "ME TOO!";
-divParent.appendChild(divParagraph)
+divParent.appendChild(divParagraph);
 
 //append div to container
-container.appendChild(divParent)
+container.appendChild(divParent);
+
+
+//button
+function alertFunction() {
+    alert("YAY! YOU DID IT!");
+  }
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', alertFunction);
+btn.addEventListener('click', function (e) {
+    console.log(e.target);
+    e.target.style.background = 'blue';
+})
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    alert(button.id);
+  });
+});
